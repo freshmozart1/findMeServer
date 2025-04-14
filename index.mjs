98312901e055b92c94f1dadfcb3da79310607dd0
freshmozart1 <78731534+freshmozart1@users.noreply.github.com>
2025-04-14 20:39:39 +0200

    Refactor Firebase configuration handling to use environment variables in production

--- index.mjs
+++ index.mjs
@@ -1,5 +1,5 @@
 import { WebSocketServer } from "ws";
-import { initializeApp, cert } from 'firebase-admin/app';
+import { initializeApp } from 'firebase-admin/app';
 import fs from 'fs';
 
 const server = new WebSocketServer({ port: 8080 });
@@ -12,6 +12,15 @@ let app;
 try {
     if (process.env.NODE_ENV === 'development') {
         process.env.FIREBASE_CONFIG = fs.readFileSync('./firebase.secret.json', 'utf8');
+    } else {
+        process.env.FIREBASE_CONFIG = {
+            apiKey: process.env.FIREBASE_API_KEY,
+            authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+            projectId: process.env.FIREBASE_PROJECT_ID,
+            storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+            messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+            appId: process.env.FIREBASE_APP_ID
+        }
     }
     app = initializeApp(JSON.parse(process.env.FIREBASE_CONFIG));
 } catch (error) {
