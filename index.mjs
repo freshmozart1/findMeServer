564ec0a01e8bc4fe8fe8bae636c73dc6c775924e
freshmozart1 <78731534+freshmozart1@users.noreply.github.com>
2025-04-23 10:23:12 +0200

    Implemented a Firestore database for persistence

--- index.mjs
+++ index.mjs
@@ -1,88 +1,120 @@
 import { WebSocketServer } from "ws";
-import { initializeApp } from 'firebase-admin/app';
+import { initializeApp } from 'firebase/app';
+import { getFirestore, collection, query, getDocs, doc, setDoc, addDoc, serverTimestamp, onSnapshot, deleteDoc } from "firebase/firestore";
 import fs from 'fs';
 
 const server = new WebSocketServer({ port: 8080 });
-const rooms = {};
-let nextConnectionId = 0;
-let activeConnectionsCount = 0;
 console.log("WebSocket server is running on ws://localhost:8080");
 
 let app;
+let db;
 try {
-    let firebaseConfig;
     if (process.env.NODE_ENV === 'development') {
-        firebaseConfig = JSON.parse(fs.readFileSync('./firebase.secret.json', 'utf8'));
+        const { apiKey, authDomain, projectId, storageBucket, messagingSenderId, appId, databaseId } = JSON.parse(fs.readFileSync('./firebase.secret.json', 'utf8'));
+        app = initializeApp({ apiKey, authDomain, projectId, storageBucket, messagingSenderId, appId });
+        db = getFirestore(app, databaseId);
     } else {
-        firebaseConfig = {
-            apiKey: process.env.FIREBASE_API_KEY,
-            authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-            projectId: process.env.FIREBASE_PROJECT_ID,
-            storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-            messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-            appId: process.env.FIREBASE_APP_ID
-        }
+        const { FIREBASE_API_KEY, FIREBASE_AUTH_DOMAIN, FIREBASE_PROJECT_ID, FIREBASE_STORAGE_BUCKET, FIREBASE_MESSAGING_SENDER_ID, FIREBASE_APP_ID, FIREBASE_DATABASE_ID } = process.env;
+        app = initializeApp({
+            apiKey: FIREBASE_API_KEY,
+            authDomain: FIREBASE_AUTH_DOMAIN,
+            projectId: FIREBASE_PROJECT_ID,
+            storageBucket: FIREBASE_STORAGE_BUCKET,
+            messagingSenderId: FIREBASE_MESSAGING_SENDER_ID,
+            appId: FIREBASE_APP_ID
+        });
+        db = getFirestore(app, FIREBASE_DATABASE_ID);
     }
-    app = initializeApp(firebaseConfig);
 } catch (error) {
-    console.error("Error initializing Firebase Admin SDK:", error);
+    console.error("Error initializing Firebase:", error);
     process.exit(1);
 }
 
+async function getRoomIfExists(roomId) {
+    if (!db || !roomId) return false;
+    try {
+        const { empty, docs } = await getDocs(query(collection(db, roomId)));
+        return empty ? false : docs;
+    } catch (error) {
+        console.error("Error getting collection:", error);
+        throw error;
+    }
+}
+
+async function joinRoom(ws, { lat, lng }) {
+    if (!db) throw new Error("Firebase not initialized");
+    try {
+        const time = new Date();
+        // await setDoc(doc(db, ws.roomId, ws.id), { createdAt: time });
+        const memberRef = await addDoc(collection(db, ws.roomId), { createdAt: time });
+        ws.id = memberRef.id;
+        return await addDoc(collection(db, ws.roomId, ws.id, 'locations'), { lat, lng, createdAt: time });
+    } catch (error) {
+        console.error("Error creating collection:", error);
+        throw error;
+    }
+}
+
+function checkAlive(ws) {
+    sendMessage(ws, 'pong');
+    clearTimeout(ws.heartbeatTimeout);
+    ws.heartbeatTimeout = setTimeout(() => ws.terminate(), 30000);
+}
+
+function generateRoomId() {
+    return Array.from({ length: 4 }, () => "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"[Math.floor(Math.random() * 62)]).join('');
+}
+
+function addLocationSnapshot(ws, memberId) {
+    ws.locationSnapshots.push(onSnapshot(collection(db, ws.roomId, memberId, 'locations'), locSnap => locSnap.docChanges().forEach(({ type, doc }) => type === 'added' && sendMessage(ws, 'location', { id: memberId, ...doc.data() }))));
+}
+
+function sendMessage(ws, type, data = undefined) {
+    ws.send(JSON.stringify(data ? { type, ...data } : { type }));
+}
+
+async function receivedMessage(ws, message) {
+    try {
+        const { type: messageType, lat, lng, roomId } = JSON.parse(message);
+        if (!messageType) throw "Message type is required";
+        if (messageType === 'ping') {
+            checkAlive(ws);
+            return;
+        };
+        if ((messageType === 'new' || messageType === 'join') && (typeof lat !== 'number' || typeof lng !== 'number')) throw "Latitude and Longitude must be numbers";
+        if (messageType === 'new') {
+            do { ws.roomId = generateRoomId(); } while (await getRoomIfExists(ws.roomId));
+            await joinRoom(ws, { lat, lng });
+            sendMessage(ws, 'room', { id: ws.roomId });
+        } else if (messageType === 'join') {
+            if (!roomId) throw "Room ID is required";
+            const room = await getRoomIfExists(roomId);
+            if (!room) throw "Room not found";
+            ws.roomId = roomId;
+            await joinRoom(ws, { lat, lng });
+        } else throw "Invalid message type";
+        if (messageType === 'new' || messageType === 'join') {
+            ws.roomSnapshot = onSnapshot(collection(db, ws.roomId), snap => snap.docChanges().forEach(({ type, doc: { id } }) => {
+                type === 'added' && id !== ws.id && addLocationSnapshot(ws, id);
+                type === 'removed' && sendMessage(ws, 'leave', { id });
+            }));
+        }
+    } catch (error) {
+        sendMessage(ws, 'error', { message: error.message || error });
+    }
+}
+
 server.on("connection", (ws) => {
-    ws.connectionId = nextConnectionId.valueOf();
-    ws.isAlive = true;
-    nextConnectionId++;
-    activeConnectionsCount++;
-
-    ws.on('pong', () => {
-        ws.isAlive = true;
-    });
-
-    ws.on('message', (data) => {
-        try {
-            const message = data.toString();
-            if (message === 'new' && !rooms[ws.connectionId]) {
-                rooms[ws.connectionId] = new Set();
-                rooms[ws.connectionId].add(ws);
-                ws.send(JSON.stringify({ connectionId: ws.connectionId }));
-            } else if (message === 'new' && rooms[ws.connectionId]) throw new Error("Room already exists");
-            else {
-                const { connectionId: roomId, lattitude, longitude } = JSON.parse(message);
-                if (!lattitude || !longitude) throw new Error("No GPS coordinates provided");
-                const room = rooms[roomId];
-                if (!room) throw new Error("Room not found");
-                if (!room.has(ws)) room.add(ws);
-                for (const client of room) client !== ws && client.send(JSON.stringify({ connectionId: roomId, lattitude, longitude }));
-            }
-        } catch (error) {
-            ws.send(error.message);
+    Object.assign(ws, { roomId: undefined, id: undefined, roomSnapshot: undefined, locationSnapshots: [], heartbeatTimeout: undefined });
+    checkAlive(ws);
+    ws.on('message', (message) => receivedMessage(ws, message));
+    ws.on('close', async () => {
+        if (ws.roomId) {
+            const memberRef = doc(db, ws.roomId, ws.id);
+            await Promise.all([...(await getDocs(query(collection(memberRef, 'locations')))).docs.map(docSnap => deleteDoc(docSnap.ref)), deleteDoc(memberRef)]);
+            ws.roomSnapshot && ws.roomSnapshot(); // unsubscribe from room snapshot
+            ws.locationSnapshots.forEach(unsub => unsub()); // unsubscribe from location snapshots
         }
-    });
-
-
-    ws.on("close", () => {
-        if (rooms[ws.connectionId]) {
-            rooms[ws.connectionId].delete(ws);
-            if (!rooms[ws.connectionId].size) delete rooms[ws.connectionId];
-        }
-        activeConnectionsCount--;
-        if (activeConnectionsCount === 0) nextConnectionId = 0;
+        clearTimeout(ws.heartbeatTimeout); // clear heartbeat timeout on disconnect
     });
 });
-
-const interval = setInterval(() => {
-    server.clients.forEach((ws) => {
-        if (ws.isAlive === false) {
-            if (rooms[ws.connectionId]) {
-                rooms[ws.connectionId].delete(ws);
-                if (!rooms[ws.connectionId].size) delete rooms[ws.connectionId];
-            }
-            return ws.terminate();
-        }
-        ws.isAlive = false;
-        ws.ping();
-    });
-}, 30000);
-
-server.on("close", () => clearInterval(interval));
